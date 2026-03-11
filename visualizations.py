2219f7370fec708c26fdb8c9161cae5bda1f826b
jsandoval11 <jjsandoval@ucdavis.edu>
2026-03-10 18:15:36 -0700

    Update visualizations.py

--- visualizations.py
+++ visualizations.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
+import seaborn as sns
 # Summary Statistics
+cat_columns = ['time_category', 'day_posted', 'media', 'attachment', 'flair', 'question']
 
+def categorical(column):
+    sns.countplot(x=column, data=reddit_posts)
+    plt.title(f'Distribution of {column}')
+    plt.show()
+
+    categorical_stats = reddit_posts[column].describe()
+    print(categorical_stats)
+
+for col in cat_columns:
+    categorical(col)
 # Correlation heatmat between variables and our response
 
 # Distribution of upvotes per posts: summary statistics
